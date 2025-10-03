9b595b29dee24ea300d35fa4751e802c6934a2fd
thatsn00pster <168500351+thatsn00pster@users.noreply.github.com>
2025-10-03 01:37:50 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thatsn00pster/Auto-Join/refs/heads/main/Chicken8.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thatsn00pster/Auto-Join/refs/heads/main/Chicken9.lua"))()
